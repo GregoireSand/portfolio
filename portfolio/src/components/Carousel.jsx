58ab695c8abf90b372cedda268dbea6d6df3f20c
Grégoire Sandrock <sandrockgregoire@gmail.com>
2024-08-29 15:11:43 +0200

    carousel & router

--- portfolio/src/components/Carousel.jsx
+++ portfolio/src/components/Carousel.jsx
@@ -1,20 +1,21 @@
 import styles from "./Carousel.module.scss";
 import works from "../datas/datas.json";
+import { Link } from "react-router-dom";
 
 function Carousel() {
-
   const cards = works.map((work) => (
-    <div className={styles.carousel} key={work.id}>
-      <ul>
-        <li>
-          <a href="" className={styles.carousel__link}>
-            <article className={styles.card__titles}>
+    <div className={styles.card} key={work.id}>
+      <Link to={`/work/${work.id}`}>
+        <img src="" alt="" />
+        <div className={styles.card__overlay}>
+          <a href="" className={styles.card__link}>
+            <span className={styles.card__titles}>
               <h3>Kasa</h3>
               <p>Site d'annonces immobilières</p>
-            </article>
+            </span>
           </a>
-        </li>
-      </ul>
+        </div>
+      </Link>
     </div>
   ));
 
---
+++ portfolio/src/components/Router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "../pages/home/Home";
+import WorkPage from "../pages/work/work";
+
+function Router() {
+  const router = createBrowserRouter([
+    { path: "/", element: <HomePage /> },
+    { path: "/work/:id", element: <WorkPage /> },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default Router;
